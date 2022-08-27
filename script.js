97fdc33ce2ae4bb319590b68c82ba9f69ee05579
Uğur Alp <38879240+UAt23@users.noreply.github.com>
2022-08-27 09:29:08 +0300

    Added project selection events

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 'use strict';
 
 ///////////////////////////////////////
+// DATA SOURCE
+let projectNames = [
+    {
+        name: 'Baking',
+        selected: false
+    },
+    {
+        name: 'Testing',
+        selected: false
+    },
+    {
+        name: 'Drawing',
+        selected: false
+    },
+    {
+        name: 'Revisiting',
+        selected: false
+    },
+]
+///////////////////////////////////////
+// VARIABLES
+let selectProject = false;
+///////////////////////////////////////
 // DOM ELEMENTS
-//const form = document.querySelector('.form');
+const projects = document.querySelector('.projects');
+for (let child of projects.children) {
+    child.addEventListener('click', () => {
+        selectProject = !selectProject;
+        selectProject 
+        ? child.classList.add('active')
+        : child.classList.remove('active')
+    });
+}
+
